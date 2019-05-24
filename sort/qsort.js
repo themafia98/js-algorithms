b6c5da1fdcdd90a2c2ca0cc75166156a275cbad1
Pavel Petrovich <gaara33377@gmail.com>
2019-05-24 23:49:29 +0300

    qsort

---
+++ sort/qsort.js
@@ -0,0 +1,21 @@
+/*
+Найти опорный элемент в массиве для сравнения с остальными частями, затем сдвигать элементы так,
+чтобы все части перед опорным элементом были меньше его, а все элементы после опорного были больше его.
+После этого рекурсивно выполнить ту же операцию на элементы до и после опорного.
+*/
+
+const qsort = arr => {
+
+    if (arr.length <= 1){
+        return arr;
+    } else {
+
+        const pivot = arr[Math.floor(arr.length/2)];
+        const left = arr.filter(element => element < pivot);
+        const right = arr.filter(element => element > pivot);
+        return [...qsort(left),pivot,...qsort(right)];
+    }
+};
+
+
+console.log(qsort([5,6,1,2,3,4]));
